7eff540775ac94752536eab8f047ff71dac3d73d
Xinyi Xiao <54774423+xiao236@users.noreply.github.com>
2022-02-07 23:52:43 -0500

    testing tkinter and moving to csv files

--- base_game.py
+++ base_game.py
@@ -3,56 +3,6 @@ import scheme
 import items
 import config
 
-def rhino_setup(mode,num_pl):
-    if mode:
-        config.main_villain.append(villain.villain("Rhino", 14 * num_pl, 2, 1))
-        config.main_villain.append(villain.villain("Rhino", 15 * num_pl, 3, 1))
-    else:
-        config.main_villain.append(villain.villain("Rhino", 15 * num_pl, 3, 1))
-        config.main_villain.append(villain.villain("Rhino", 16 * num_pl, 4, 1))
-    config.main_scheme.append(scheme.scheme("The Break-In!", 0, num_pl, 7 * num_pl))
-    config.possible_items(items.items("Armored Rhino Suit", 0, 0))
-    config.possible_items(items.items("Charge", 3, 0))
-    config.possible_items(items.items("Enhanced Ivory Horn", 1, 0))
-    config.possible_enemies(villain.villain("Hydra Mercenary", 3, 1, 0))
-    config.possible_enemies(villain.villain("Sandman", 4, 3, 2))
-    config.possible_enemies(villain.villain("Shocker", 3, 2, 1))
-    config.possible_schemes(scheme.scheme("Breakin' & Takin'", 2+num_pl,0,0))
-    config.possible_schemes(scheme.scheme("Crowd Control", 2*num_pl, 0, 0))
-
-def klaw_setup(mode,num_pl):
-    if mode:
-        config.main_villain.append(villain.villain("Klaw",12*num_pl,0,2))
-        config.main_villain.append(villain.villain("Klaw",18*num_pl,1,2))
-    else:
-        config.main_villain.append(villain.villain("Klaw", 18 * num_pl, 1,2))
-        config.main_villain.append(villain.villain("Klaw", 22 * num_pl, 2,3))
-    config.main_scheme.append(scheme.scheme("Underground Distribution", 0, num_pl, 6 * num_pl))
-    config.main_scheme.append(scheme.scheme("Secret Rendezvous", 0, num_pl, 8 * num_pl))
-    config.possible_items.append(items.items("Sonic Converter", 1, 0))
-    config.possible_items.append(items.items("Solid-Sound Body", 0, 0))
-    config.possible_enemies.append(villain.villain("Armored Guard",3,1,0))
-    config.possible_enemies.append(villain.villain("Weapons Runner", 2, 1, 1))
-    config.possible_schemes.append(scheme.scheme("Illegal Arms Factory",3+num_pl,0,0))
-    config.possible_schemes.append(scheme.scheme("The \"Immortal\" Klaw", 3 * num_pl, 0,0))
-
-def ultron_setup(mode, num_pl):
-    if mode:
-        config.main_villain.append(villain.villain("Ultron", 17*num_pl, 2, 1))
-        config.main_villain.append(villain.villain("Ultron", 22 * num_pl, 2, 2))
-    else:
-        config.main_villain.append(villain.villain("Ultron", 22 * num_pl, 2, 2))
-        config.main_villain.append(villain.villain("Ultron", 27 * num_pl, 4, 2))
-    config.main_scheme.append(scheme.scheme("The Crimson Cowl", 0, num_pl, 3 * num_pl))
-    config.main_scheme.append(scheme.scheme("Assault on NORAD", 0, num_pl, 10 * num_pl))
-    config.main_scheme.append(scheme.scheme("Countdown to Oblivion", 0, num_pl, 5 * num_pl))
-    config.possible_items.append(items.items("Program Transmitter", 0, 1))
-    config.possible_items.append(items.items("Upgraded Drones", 0, 0))
-    config.possible_enemies.append(villain.villain("Advanced Ultron Drone", 4, 1, 1))
-    config.possible_schemes.append(scheme.scheme("Drone Factory", 4, 0, 0))
-    config.possible_schemes.append(scheme.scheme("Invasive AI", 3*num_pl, 0, 0))
-    config.possible_schemes.append(scheme.scheme("Ultron's Imperative", 2 * num_pl, 0, 0))
-
 def risky_business_setup(mode, num_pl):
     if mode:
         config.main_villain.append(villain.villain("Green Goblin", 14*num_pl, 3, 2))
@@ -87,32 +37,6 @@ def mutagen_formula_setup(mode, num_pl):
     config.possible_schemes.append(scheme.scheme("Goblin Reinforcements", 2, 0, 0))
     config.possible_schemes.append(scheme.scheme("Goblin Nation", 2*num_pl, 0, 0))
     config.possible_schemes.append(scheme.scheme("Overrun", num_pl, 0, 0))
-
-def add_LoH():
-    config.possible_schemes.append(scheme.scheme("Legions of Hydra", 3, 0, 0))
-    config.possible_enemies.append(villain.villain("Madame Hydra", 6, 2, 2))
-    config.possible_enemies.append(villain.villain("Hydra Soldier", 4, 2, 1))
-
-def add_MoE(num_pl):
-    config.possible_schemes.append(scheme.scheme("The Masters of Evil", 3*num_pl, 0, 0))
-    config.possible_enemies.append(villain.villain("Radioactive Man", 7, 1, 1))
-    config.possible_enemies.append(villain.villain("Whirlwind", 6, 2, 1))
-    config.possible_enemies.append(villain.villain("Tiger Shark", 6, 3, 1))
-    config.possible_enemies.append(villain.villain("Melter", 5, 3, 1))
-
-def add_BS(num_pl):
-    config.possible_schemes.append(scheme.scheme("Bomb Scare", 2+num_pl, 0, 0))
-    config.possible_enemies.append(villain.villain("Hydra Bomber", 2, 1, 1))
-
-def add_UA():
-    config.possible_schemes.append(scheme.scheme("Under Attack", 3, 0, 0))
-    config.possible_items.append(items.items("Vibranium Armor", 0, 0))
-    config.possible_items.append(items.items("Concussion Blasters", 1, 0))
-
-def add_tDC():
-    config.possible_schemes.append(scheme.scheme("The Doomsday Chair", 8, 0, 0))
-    config.possible_enemies.append(villain.villain("MODOK", 8, 2, 2))
-    config.possible_items.append(items.items("Biomechanical Upgrades", 0, 0))
 
 def add_GG():
     config.possible_items.append(items.items("Goblin Glider", 1, 0))
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 active_enemies = []
 active_schemes = []
 active_items = []
@@ -8,3 +10,14 @@ possible_items = []
 
 main_villain = []
 main_scheme = []
+
+def clear():
+    possible_schemes.clear()
+    possible_enemies.clear()
+    possible_items.clear()
+    main_villain.clear()
+    main_scheme.clear()
+
+def parse_cards(sets, num_pl, mode):
+    vil_df = pd.read_csv('enemies.csv')
+    vil_df = vil_df[vil_df['set'] in sets]
